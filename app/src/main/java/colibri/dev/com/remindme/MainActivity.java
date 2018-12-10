46b49a614770abe7d5831d92e3256eaf789cbc6e
andrblag@gmail.com <galinka89>
2018-12-10 15:05:53 +0200

    Created Toolbar

--- app/src/main/java/colibri/dev/com/remindme/MainActivity.java
+++ app/src/main/java/colibri/dev/com/remindme/MainActivity.java
@@ -1,13 +1,36 @@
 package colibri.dev.com.remindme;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private android.support.v7.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+
     }
+
+    private void initToolbar(){
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+
+        toolbar.setOnMenuItemClickListener(new android.support.v7.widget.Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
+    }
+
+
 }
